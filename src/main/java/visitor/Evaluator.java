07d9916ea95a2f1d390d7eced80650516690bcd9
NinjaPhillipe <23127972+NinjaPhillipe@users.noreply.github.com>
2021-04-29 17:43:53 +0200

    javadoc

--- src/main/java/visitor/Evaluator.java
+++ src/main/java/visitor/Evaluator.java
@@ -3,9 +3,18 @@ package visitor;
 import calculator.MyNumber;
 import calculator.exceptions.ComputeError;
 
+/**
+ * Visitor who goes through an expression to evaluate it.
+ */
 public abstract class Evaluator extends Visitor{
     private MyNumber computedValue;
     private ComputeError exception; // if the expression throw an exception
+
+    /**
+     * Return result of evaluation or throw an exception if an error has been detected
+     * @return value of evaluation
+     * @throws ComputeError
+     */
     public MyNumber getResult() throws ComputeError {
         if ( exception != null) throw  exception;
         return computedValue;
